7a98d55969a4122248a94c968fef2c2ec7a6ebf9
G-KrishnaAdithya <krishnaadithyag@gmail.com>
2023-07-23 18:21:27 +0530

    updated ask a query

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from website import create_app
 app=create_app()
 
 if __name__=='__main__':
-    app.run(port=8080)
+    app.run(debug=True)
 
--- website/auth.py
+++ website/auth.py
@@ -389,7 +389,8 @@ def signup():
 def ask():
     return render_template("takeq.html")
 
-openai.api_key = "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"
+#openai.api_key = "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"
+openai.api_key ="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"
 messages = [{"role": "system", "content": "You are a computer science expert"}]
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
