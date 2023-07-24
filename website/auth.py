ffccadb8139547fc0a3f17c5a82f011c54459d7f
G-KrishnaAdithya <krishnaadithyag@gmail.com>
2023-07-24 07:36:12 +0530

    updated style for result

--- website/auth.py
+++ website/auth.py
@@ -389,7 +389,7 @@ def signup():
 def ask():
     return render_template("takeq.html")
 
-openai.api_key ="your-open-ai-api-key"
+openai.api_key ="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"
 messages = [{"role": "system", "content": "You are a computer science expert"}]
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
